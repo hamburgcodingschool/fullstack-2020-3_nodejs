32d4aec1bab83112fc77b9334d10fd33963feb6f
Helder Pereira <helder.pereira@xing.com>
2020-10-26 18:47:15 +0100

    update

--- code/lesson3/n2-crudServer/index.js
+++ code/lesson3/n2-crudServer/index.js
@@ -16,6 +16,8 @@ app.get('/', function(req, res) {
 
 app.post('/newPerson', function(req, res) {
 
+    console.log(req.body);
+
     const newPerson = req.body;
 
     data.push(newPerson);
@@ -29,8 +31,37 @@ app.delete('/delete/:id', function(req, res) {
 
     const id = req.params.id; // id is the array Position, now I want to remove that element from the array...
 
-    
+    if (data[id]) {
+        data.splice(id, 1);
+        res.json({
+            "status": "ok",
+            "description": "deleted"
+        });
+    } else {
+        res.json({
+            "status": "error",
+            "description": "id not found"
+        });
+    }
 })
+
+app.put('/update/:id', function(req, res) {
+    //replacing the Object
+    const id = req.params.id; // id is the array Position
+
+    if (data[id]) {
+        data[id] = req.body;
+        res.json({
+            "status": "ok",
+            "description": "updated"
+        });
+    } else {
+        res.json({
+            "status": "error",
+            "description": "id not found"
+        });
+    }
+});
 
 app.listen(port, function() {
     console.log(`Running on http://localhost:${port}`);
